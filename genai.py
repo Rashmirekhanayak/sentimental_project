a3b7b3c26a5751bd14cd63127d8cf116f88efb57
Rashmirekha nayak <nayakrashmirekha307@gmail.com>
2025-04-16 10:31:32 -0700

    Added all project files

---
+++ genai.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key = "AIzaSyD4XybnbHn3rSH_99O8SZadZxxQv2ig4fE")
+model = genai.GenerativeModel('gemini-1.5-flash')  
+
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+    st.session_state.messages = []
+
+st.set_page_config(page_title="Gemini Chatbot", page_icon="🤖", layout="centered")
+st.title("🤖 Gemini AI Chatbot")
+
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+user_input = st.chat_input("Ask something...")
+if user_input:
+
+    st.chat_message("user").markdown(user_input)
+    st.session_state.messages.append({"role": "user", "content": user_input})
+
+
+    try:
+        response = st.session_state.chat.send_message(user_input)
+        answer = response.text
+    except Exception as e:
+        answer = f"⚠ Error: {str(e)}"
+
+    st.chat_message("assistant").markdown(answer)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
